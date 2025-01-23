4f54c5057b0d648a553818e9c39fb8df13ed83e1
fadhluibnu <f.ibnu.a1@gmail.com>
2025-01-23 19:23:38 +0700

    add: Section Kontak & Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-3 gap-5">
+      <div className="flex flex-col gap-5 col-span-2">
+        <div className="flex gap-3 align-center">
+          <img src="assets/Logo.png" alt="logo" className="w-9 h-9" />
+          <h1 className="text-lg font-semibold font-montserrat text-white p-0">
+            InSoft.
+          </h1>
+        </div>
+        <h1 className="text-white text-[64px] leading-[62px] font-medium">
+          Hadirkan{" "}
+          <span className="underline decoration-[#2C57F4]">
+            Keindahan dan Kenyamanan
+          </span>{" "}
+          di Rumah Anda
+        </h1>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-white font-semibold font-montserrat text-[26px] underline">
+            Menu
+          </h1>
+          <ul className="flex flex-col gap-1">
+            <li className="relative">
+              <a
+                href="#"
+                className="navbar-item text-white font-medium font-poppins text-sm uppercase"
+              >
+                beranda
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="navbar-item text-white font-medium font-poppins text-sm uppercase"
+              >
+                tentang kami
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="navbar-item text-white font-medium font-poppins text-sm uppercase"
+              >
+                produk
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="navbar-item text-white font-medium font-poppins text-sm uppercase"
+              >
+                testimoni
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="navbar-item text-white font-medium font-poppins text-sm uppercase"
+              >
+                lokasi
+              </a>
+            </li>
+            <li className="relative">
+              <a
+                href="#"
+                className="navbar-item text-white font-medium font-poppins text-sm uppercase"
+              >
+                kontak
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-white font-semibold font-montserrat text-[26px] underline">
+            Media Sosial
+          </h1>
+          <div className="flex gap-3">
+            <a href="">
+              <img src="assets/whatsapp.svg" alt="whatsapp" />
+            </a>
+            <a href="">
+              <img src="assets/facebook.svg" alt="facebook" />
+            </a>
+            <a href="">
+              <img src="assets/x.svg" alt="x" />
+            </a>
+            <a href="">
+              <img src="assets/linkedin.svg" alt="linkedin" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="container font-poppins">
+    <div className="font-poppins">
       <Navbar />
 
       {/* Hero Section */}
@@ -30,26 +31,30 @@ export default function Home() {
       </section>
 
       {/* Testimoni Section */}
-      <section className="pt-5 pb-64 relative">
+      <section className="pt-5 pb-64 relative w-full right-0">
         <div>
-          <img src="assets/bg.png" alt="" />
+          <img className="w-full" src="assets/bg.png" alt="" />
         </div>
-        <div className="absolute bottom-16 px-[75px] py-5 flex gap-1">
-          <div className="bg-black text-white w-[374px] py-[46px] flex flex-col justify-center items-center">
-            <h1 className="text-[68px] font-bold">8+</h1>
-            <p className="text-[20px] font-semibold font-montserrat">Produk</p>
-          </div>
-          <div className="bg-black text-white w-[374px] py-[46px] flex flex-col justify-center items-center">
-            <h1 className="text-[68px] font-bold">1000+</h1>
-            <p className="text-[20px] font-semibold font-montserrat">
-              Unit Produk Terjual
-            </p>
-          </div>
-          <div className="bg-black text-white w-[374px] py-[46px] flex flex-col justify-center items-center">
-            <h1 className="text-[68px] font-bold">5/5</h1>
-            <p className="text-[20px] font-semibold font-montserrat">
-              Rating Google
-            </p>
+        <div className="absolute bottom-28 w-full flex px-[75px]">
+          <div className="w-full py-5 flex gap-1 m-auto justify-center items-center">
+            <div className="bg-black text-white w-[374px] py-[46px] flex flex-col justify-center items-center">
+              <h1 className="text-[68px] font-bold">8+</h1>
+              <p className="text-[20px] font-semibold font-montserrat">
+                Produk
+              </p>
+            </div>
+            <div className="bg-black text-white w-[374px] py-[46px] flex flex-col justify-center items-center">
+              <h1 className="text-[68px] font-bold">1000+</h1>
+              <p className="text-[20px] font-semibold font-montserrat">
+                Unit Produk Terjual
+              </p>
+            </div>
+            <div className="bg-black text-white w-[374px] py-[46px] flex flex-col justify-center items-center">
+              <h1 className="text-[68px] font-bold">5/5</h1>
+              <p className="text-[20px] font-semibold font-montserrat">
+                Rating Google
+              </p>
+            </div>
           </div>
         </div>
       </section>
@@ -83,7 +88,8 @@ export default function Home() {
               <div>
                 <h1 className="font-medium text-[26px]">Visi</h1>
                 <p className="text-sm text-grey1 font-montserrat tracking-wide leading-tight text-justify">
-                  Menjadi pelopor dalam industri furnitur dengan menghadirkan desain inovatif yang memenuhi kebutuhan setiap rumah.
+                  Menjadi pelopor dalam industri furnitur dengan menghadirkan
+                  desain inovatif yang memenuhi kebutuhan setiap rumah.
                 </p>
               </div>
               <div>
@@ -106,6 +112,86 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </section>
+
+      {/* Kontak Section */}
+      <section className="w-full bg-black p-[75px] flex flex-col gap-16">
+        <div id="kontak" className="grid grid-cols-2 gap-10">
+          <div className="flex gap-[20px]">
+            <div className="p-3 mt-4 h-6 bg-white"></div>
+            <div className="flex gap-3 flex-col">
+              <h1 className="font-semibold text-white text-[44px] font-montserrat uppercase">
+                Hubungi Kami
+              </h1>
+              <p className="text-sm text-white font-poppins text-justify">
+                Kami bangga telah menjadi bagian dari perjalanan banyak
+                pelanggan untuk menciptakan ruang impian mereka. Inilah beberapa
+                cerita dan ulasan dari mereka yang telah merasakan kualitas
+                produk dan layanan kami
+              </p>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.772802288226!2d109.59421807501859!3d-7.7075120763473715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7acbccdabcbe0b%3A0x1280deb733d08431!2sFadhlu%20Ibnu!5e0!3m2!1sid!2sid!4v1737617674162!5m2!1sid!2sid"
+                className="w-full"
+                height="450"
+                style={{ border: 0 }}
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+              <div>
+                <p className="font-medium font-poppins text-white">
+                  📍 Kunjungi showroom atau kantor kami di:
+                </p>
+                <p className="text-sm text-white font-poppins">
+                  Jl. Bukit Lestari No. 45, Kelurahan Sekaran, Kecamatan
+                  Gunungpati, Kota Semarang, Jawa Tengah 50229
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-5 p-10 bg-[#1E1E1E] h-fit">
+            <h1 className="font-medium text-[26px] text-white">
+              Formulir Kontak
+            </h1>
+            <p className="text-sm text-white font-poppins text-justify">
+              Isi formulir di bawah ini, dan tim kami akan segera menghubungi
+              Anda:
+            </p>
+            <form className="flex flex-col gap-3">
+              <div className="flex flex-col gap-1">
+                <label className="text-sm text-white font-poppins">Nama</label>
+                <input
+                  placeholder="Masukkan nama anda"
+                  className="bg-transparent p-3 text-sm text-white font-poppins border-white border outline-white"
+                  type="text"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label className="text-sm text-white font-poppins">
+                  Email atau Nomor WhatsApp
+                </label>
+                <input
+                  placeholder="Masukkan email atau nomor whatsapp anda"
+                  className="bg-transparent p-3 text-sm text-white font-poppins border-white border outline-white"
+                  type="text"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label className="text-sm text-white font-poppins">Pesan</label>
+                <textarea
+                  placeholder="Masukkan pesan anda"
+                  className="bg-transparent p-3 text-sm text-white font-poppins border-white border outline-white"
+                  type="text"
+                />
+              </div>
+              <button className="bg-black text-white font-poppins py-3 focus:border-white focus:border transition-all duration-300">
+                Kirim Pesan
+              </button>
+            </form>
+          </div>
+        </div>
+        <hr />
+        <Footer />
       </section>
     </div>
   );
